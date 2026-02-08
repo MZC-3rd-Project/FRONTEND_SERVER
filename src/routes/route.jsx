ae374da02b8f4afe4e61390eea6aeafca0256f60
waney <ssdii66@gmail.com>
2026-02-08 18:51:34 +0900

    [main] domain 파일 구조 정의

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -1,11 +1,15 @@
 import {Routes, Route} from "react-router";
 import MainLayout from "@/components/layout/MainLayout.jsx";
+import Home from "@/domains/home/page/Home.jsx";
 
 
 function CommonRoute(){
     return (<>
         <Routes>
-            <Route path="/" element={<MainLayout/>} />
+            <Route element={<MainLayout/>} >
+                <Route path="/" element={<Home/>}/>
+                <Route path="/sub" element={<div>sub</div>}></Route>
+            </Route>
         </Routes>
     </>);
 }
