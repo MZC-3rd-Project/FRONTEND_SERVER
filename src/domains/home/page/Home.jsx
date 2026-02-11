d614ddc424cdf24c11d4ed1bd7cf52cc961aa208
waney <ssdii66@gmail.com>
2026-02-11 11:12:21 +0900

    [main] shadcn/ui 추가

--- src/domains/home/page/Home.jsx
+++ src/domains/home/page/Home.jsx
@@ -1,7 +1,10 @@
+import {Input} from "@/components/ui/input";
 
 function Home() {
     return (
-        <div>home</div>
+        <div>
+            <Input type="text" className="outline-1" placeholder="입력"></Input>
+        </div>
     )
 }
 
